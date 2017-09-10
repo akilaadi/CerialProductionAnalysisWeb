f7409a0585a08a388fd45c75dfd43fdce394ec62
Akila Adikari <akila.adi@gmail.com>
2017-09-10 10:30:22 +0530

    Add project files.

---
+++ Dashboard/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+<html>
+    <head></head>
+    <body>
+        <h2>Cerial Production effectivenes (M.T per Hect)</h2>
+        <asp:Chart ID="Chart1" runat="server" Width="1000px" Height="500px">
+            <Series>
+                <asp:Series Name="Series1">
+                </asp:Series>
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1">
+                    <AxisX IsLabelAutoFit="true" LabelAutoFitMaxFontSize="10" Title="Cerial Product" Interval="1">
+                        <LabelStyle Angle="90" IsStaggered="false"/>
+                    </AxisX>
+                    <AxisY Title="M.T per Hect">
+
+                    </AxisY>
+                </asp:ChartArea>
+            </ChartAreas>
+        </asp:Chart>
+    </body>
+</html>
+
---
+++ Dashboard/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.DataVisualization.Charting;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string url = "http://ec2-54-254-220-66.ap-southeast-1.compute.amazonaws.com:50075/webhdfs/v1/user/hduser/cerialProductionAnalysis-output/part-r-00000?op=OPEN&namenoderpcaddress=master:54310&offset=0";
+        WebClient myWebClient = new WebClient();
+        try
+        {
+
+            Dictionary<string, float> allLinesText = new Dictionary<string, float>();
+
+            //Create a WebRequest to get the file
+            HttpWebRequest fileReq = (HttpWebRequest)HttpWebRequest.Create(url);
+
+            //Create a response for this request
+            HttpWebResponse fileResp = (HttpWebResponse)fileReq.GetResponse();
+
+            if (fileReq.ContentLength > 0)
+                fileResp.ContentLength = fileReq.ContentLength;
+
+            //Get the Stream returned from the response
+            using (Stream stream = fileResp.GetResponseStream()) 
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    allLinesText.Add(line.Split('\t')[0], float.Parse(line.Split('\t')[1]));
+                }
+            }
+
+            if (allLinesText.Count > 0)
+            {
+                Chart1.DataSource = allLinesText;
+                Chart1.Series["Series1"].XValueMember = "Key";
+                Chart1.Series["Series1"].YValueMembers = "Value";
+                Chart1.DataBind();
+            }
+        }
+        catch (Exception ex)
+        { }
+
+    }
+}
