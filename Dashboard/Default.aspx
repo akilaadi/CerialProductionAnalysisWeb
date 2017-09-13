76f0fba09a480ded0da458991cf76f00a14fe98d
Akila Adikari <akila.adi@gmail.com>
2017-09-13 16:07:52 +0530

    Added job 2

--- Dashboard/Default.aspx
+++ Dashboard/Default.aspx
@@ -2,9 +2,10 @@
 
 <%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
 <html>
-    <head></head>
-    <body>
-        <h2>Cerial Production effectivenes (M.T per Hect)</h2>
+<head></head>
+<body>
+    <form runat="server">
+        <h2>Cerial Production Effectivenes (M.T per Hect)</h2>
         <asp:Chart ID="Chart1" runat="server" Width="1000px" Height="500px">
             <Series>
                 <asp:Series Name="Series1">
@@ -13,14 +14,33 @@
             <ChartAreas>
                 <asp:ChartArea Name="ChartArea1">
                     <AxisX IsLabelAutoFit="true" LabelAutoFitMaxFontSize="10" Title="Cerial Product" Interval="1">
-                        <LabelStyle Angle="90" IsStaggered="false"/>
+                        <LabelStyle Angle="90" IsStaggered="false" />
                     </AxisX>
                     <AxisY Title="M.T per Hect">
-
                     </AxisY>
                 </asp:ChartArea>
             </ChartAreas>
         </asp:Chart>
-    </body>
+        <hr />
+        <h2>Per District Production Effectiveness (M.T per Hect)</h2>
+        <span>Select Cereal Product:</span><asp:DropDownList AutoPostBack="true" ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList>
+        <asp:Chart ID="Chart2" runat="server" Width="1000px" Height="500px">
+            <Series>
+                <asp:Series Name="Series1" IsValueShownAsLabel="true" LabelFormat="F2">
+                </asp:Series>
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1">
+                    <AxisX IsLabelAutoFit="true" LabelAutoFitMaxFontSize="10" Title="District" Interval="1">
+                        <LabelStyle Angle="90" IsStaggered="false" />
+                    </AxisX>
+                    <AxisY Title="M.T per Hect">
+                    </AxisY>
+                </asp:ChartArea>
+            </ChartAreas>
+        </asp:Chart>
+    </form>
+</body>
+
 </html>
 
--- Dashboard/Default.aspx.cs
+++ Dashboard/Default.aspx.cs
@@ -10,38 +10,29 @@ using System.Web.UI.DataVisualization.Charting;
 
 public partial class _Default : Page
 {
+    public Dictionary<string, float> job1Result;
+    public Dictionary<string, float> job2Result;
+
+    string url1 = "http://ec2-13-228-183-9.ap-southeast-1.compute.amazonaws.com:50075/webhdfs/v1/user/hduser/output/job1/part-r-00000?op=OPEN&namenoderpcaddress=master:54310&offset=0";
+    string url2 = "http://ec2-13-228-183-9.ap-southeast-1.compute.amazonaws.com:50075/webhdfs/v1/user/hduser/output/job2/part-r-00000?op=OPEN&namenoderpcaddress=master:54310&offset=0";
+
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        string url = "http://ec2-54-254-220-66.ap-southeast-1.compute.amazonaws.com:50075/webhdfs/v1/user/hduser/cerialProductionAnalysis-output/part-r-00000?op=OPEN&namenoderpcaddress=master:54310&offset=0";
-        WebClient myWebClient = new WebClient();
         try
         {
+            job1Result = ReadJobResult(url1);
+            job2Result = ReadJobResult(url2);
 
-            Dictionary<string, float> allLinesText = new Dictionary<string, float>();
-
-            //Create a WebRequest to get the file
-            HttpWebRequest fileReq = (HttpWebRequest)HttpWebRequest.Create(url);
-
-            //Create a response for this request
-            HttpWebResponse fileResp = (HttpWebResponse)fileReq.GetResponse();
-
-            if (fileReq.ContentLength > 0)
-                fileResp.ContentLength = fileReq.ContentLength;
-
-            //Get the Stream returned from the response
-            using (Stream stream = fileResp.GetResponseStream()) 
-            using (var reader = new StreamReader(stream))
+            if (!this.IsPostBack)
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    allLinesText.Add(line.Split('\t')[0], float.Parse(line.Split('\t')[1]));
-                }
+                DataBindProductDropDown();
+                DataBindChart2();
             }
 
-            if (allLinesText.Count > 0)
+            if (job1Result.Count > 0)
             {
-                Chart1.DataSource = allLinesText;
+                Chart1.DataSource = job1Result;
                 Chart1.Series["Series1"].XValueMember = "Key";
                 Chart1.Series["Series1"].YValueMembers = "Value";
                 Chart1.DataBind();
@@ -50,5 +41,73 @@ public partial class _Default : Page
         catch (Exception ex)
         { }
 
+    }
+
+    private Dictionary<string, float> ReadJobResult(string resultUrl)
+    {
+        WebClient myWebClient = new WebClient();
+        try
+        {
+            Dictionary<string, float> jobResult = new Dictionary<string, float>();
+
+            //Create a WebRequest to get the file
+            HttpWebRequest fileReq = (HttpWebRequest)HttpWebRequest.Create(resultUrl);
+
+            //Create a response for this request
+            HttpWebResponse fileResp = (HttpWebResponse)fileReq.GetResponse();
+
+            if (fileReq.ContentLength > 0)
+                fileResp.ContentLength = fileReq.ContentLength;
+
+            //Get the Stream returned from the response
+            using (Stream stream = fileResp.GetResponseStream())
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    jobResult.Add(line.Split('\t')[0], float.Parse(line.Split('\t')[1]));
+                }
+            }
+
+            return jobResult;
+
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+    public void DataBindChart2()
+    {
+        if (job2Result != null && job2Result.Count > 0)
+        {
+            Dictionary<string, float> selected = new Dictionary<string, float>();
+            job2Result.Where(t => t.Key.Split('-')[0].Equals(DropDownList1.SelectedValue))
+                .OrderBy(t=>t.Value)
+                .ToList()
+                .ForEach(t =>
+                {
+                    selected.Add(t.Key.Split('-')[1], t.Value);
+                });
+
+            Chart2.DataSource = selected;
+            Chart2.Series["Series1"].XValueMember = "Key";
+            Chart2.Series["Series1"].YValueMembers = "Value";
+            Chart2.DataBind();
+        }
+    }
+
+    public void DataBindProductDropDown()
+    {
+        string[] districtList = job2Result.Select(t => t.Key.Split('-')[0]).Distinct().ToArray();
+        DropDownList1.DataSource = districtList;
+        DropDownList1.DataBind();
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DataBindChart2();
     }
 }
